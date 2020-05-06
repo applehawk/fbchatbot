4c6d3815a420d2c0610daa8c099ff8965a958191
applehawk <ivlad.skipper@gmail.com>
2020-05-06 16:45:19 +0500

    Clean text,

--- bot.js
+++ bot.js
@@ -6,12 +6,8 @@
 
 // Import Botkit's core features
 const { Botkit } = require('botkit');
-// const { BotkitCMSHelper } = require('botkit-plugin-cms');
-
 // Import a platform-specific adapter for facebook.
-
 const { FacebookAdapter, FacebookEventTypeMiddleware } = require('botbuilder-adapter-facebook');
-
 const { MongoDbStorage } = require('botbuilder-storage-mongodb');
 
 // Load process.env values from .env file
@@ -24,12 +20,9 @@ if (process.env.MONGO_URI) {
     });
 }
 
-
 const adapter = new FacebookAdapter({
-
     // REMOVE THIS OPTION AFTER YOU HAVE CONFIGURED YOUR APP!
     enable_incomplete: true,
-
     verify_token: process.env.FACEBOOK_VERIFY_TOKEN,
     access_token: process.env.FACEBOOK_ACCESS_TOKEN,
     app_secret: process.env.FACEBOOK_APP_SECRET,
@@ -38,21 +31,24 @@ const adapter = new FacebookAdapter({
 // emit events based on the type of facebook event being received
 adapter.use(new FacebookEventTypeMiddleware());
 
-
 const controller = new Botkit({
     webhook_uri: '/api/messages',
-
     adapter: adapter,
-
     storage
 });
 
-/*if (process.env.CMS_URI) {
-    controller.usePlugin(new BotkitCMSHelper({
-        uri: process.env.CMS_URI,
-        token: process.env.CMS_TOKEN,
-    }));
-}*/
+controller.hears('hello','direct_message', function(bot, message) {
+    bot.reply(message,'Hello yourself!');
+});
+/* 
+let { Botkit } = require('botkit');
+
+const controller = new Botkit(MY_CONFIGURATION);
+
+controller.hears('hello','direct_message', function(bot, message) {
+    bot.reply(message,'Hello yourself!');
+});
+*/
 
 controller.ready(() => {
 	controller.on('message', async(bot, message) => {
@@ -61,9 +57,7 @@ controller.ready(() => {
 });
 
 controller.webserver.get('/', (req, res) => {
-
     res.send(`This app is running Botkit ${ controller.version }.`);
-
 });
 
 
