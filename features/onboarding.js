d16b9e1b528f48784a3c08cd77f511652c9e8714
applehawk <ivlad.skipper@gmail.com>
2020-06-28 08:21:08 +0300

    change to cloud

--- features/onboarding.js
+++ features/onboarding.js
@@ -384,9 +384,9 @@ module.exports = async (controller) => {
                     attachment: {
                         type: 'image',
                         payload: {
-                            attachment_id: process.env.GIF_END,
-                            // url: GIF_ONBOARDING,
-                            // is_reusable: true,
+                            //attachment_id: process.env.GIF_END,
+                            url: GIF_ONBOARDING,
+                            is_reusable: true,
                         },
                     },
                 },
