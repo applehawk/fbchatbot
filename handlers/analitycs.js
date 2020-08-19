271e6106418ade2fb3989a0038745d5ea43f2d51
lionstuff <lionstuff73@gmail.com>
2020-08-19 17:13:27 +0400

    Added Amplitude analitycs

--- handlers/analitycs.js
+++ handlers/analitycs.js
@@ -1,9 +1,19 @@
 'use strict';
+const Amplitude = require('@amplitude/node');
 
 module.exports = async (controller) => {
   controller.on(['ANALYTICS_EVENT'], async (bot, message) => {
-    if ((message.type === 'message' || message.type === 'facebook_postback' || message.type === 'messaging_postback')) {
+    // if ((message.type === 'message' || message.type === 'facebook_postback' || message.type === 'messaging_postback')) {
       console.log(`[ANALYTICS_EVENT]: ${message.type}${!!message.value ? ' ' + message.value : ''} ${message.text}`);
+
+      let client = Amplitude.init(process.env.ANALYTICS_API_KEY);
+      client.logEvent({
+        event_type: !!message.value ? message.value : message.type,
+        user_id: message.sender.id,
+        event_properties: {
+          text: message.text,
+        }
+      });
 
       const options = {
         event: 'CUSTOM_APP_EVENTS',
@@ -19,6 +29,6 @@ module.exports = async (controller) => {
         page_scoped_user_id: message.user,
       };
       const status = await bot.api.callAPI(`/${process.env.FACEBOOK_APPID}/activities`, 'POST', options);
-    }
+    // }
   });
 };
